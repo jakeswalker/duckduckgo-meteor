2725c19a6af6b446087ea9881f50a869074df10b
Jake Walker <122703805+jakeswalker@users.noreply.github.com>
2023-03-05 12:34:59 -1000

    Cleaned up the UI code

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -1,50 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Meteor } from 'meteor/meteor';
-import { useTracker } from 'meteor/react-meteor-data';
 import { Roles } from 'meteor/alanning:roles';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Landing from '../pages/Landing';
-import ListStuff from '../pages/ListStuff';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
-import AddStuff from '../pages/AddStuff';
-import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
-import SignUp from '../pages/SignUp';
-import SignOut from '../pages/SignOut';
-import SignIn from '../pages/SignIn';
-import NotAuthorized from '../pages/NotAuthorized';
 import LoadingSpinner from '../components/LoadingSpinner';
 import DuckDuckGo from '../pages/DuckDuckGo';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
-const App = () => {
-  const { ready } = useTracker(() => {
-    const rdy = Roles.subscription.ready();
-    return {
-      ready: rdy,
-    };
-  });
-  return (
-    <Router>
-      <div className="d-flex flex-column min-vh-100">
-        <Routes>
-          <Route exact path="/" element={<DuckDuckGo />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/signout" element={<SignOut />} />
-          <Route path="/home" element={<ProtectedRoute><Landing /></ProtectedRoute>} />
-          <Route path="/list" element={<ProtectedRoute><ListStuff /></ProtectedRoute>} />
-          <Route path="/add" element={<ProtectedRoute><AddStuff /></ProtectedRoute>} />
-          <Route path="/edit/:_id" element={<ProtectedRoute><EditStuff /></ProtectedRoute>} />
-          <Route path="/admin" element={<AdminProtectedRoute ready={ready}><ListStuffAdmin /></AdminProtectedRoute>} />
-          <Route path="/notauthorized" element={<NotAuthorized />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </div>
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <div className="d-flex flex-column min-vh-100">
+      <Routes>
+        <Route exact path="/" element={<DuckDuckGo />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  </Router>
+);
 
 /*
  * ProtectedRoute (see React Router v6 sample)
